2e1e39cf4cb24a0da3ac0b09b649c418ae973029
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-15 14:34:16 +0500

    Chnaged variable names according to the convention!

--- db/migrate/20220914143405_create_posts.rb
+++ db/migrate/20220914143405_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :"Title"
-      t.text :"Text"
-      t.integer :"CommentsCounter"
-      t.integer :"Likescounter"
+      t.string :"title"
+      t.text :"text"
+      t.integer :"commentsCounter"
+      t.integer :"likesCounter"
       t.references :author, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
