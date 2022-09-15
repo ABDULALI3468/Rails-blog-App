9936f694a821cbcd139cad3d06e7bcdf203e5f6c
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-15 13:00:29 +0500

    Created migrations added Foreign keys.

--- db/migrate/20220914143405_create_posts.rb
+++ db/migrate/20220914143405_create_posts.rb
@@ -5,6 +5,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :"Text"
       t.integer :"CommentsCounter"
       t.integer :"Likescounter"
+      t.references :author, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
     end
