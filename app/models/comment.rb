107245e4003a8a66416abfc8abccf947a1844830
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-16 19:18:00 +0500

    Applied CRUD operations and create custom model methods.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,16 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  after_create :increment_comments_counter
+
+  validates :text, presence: true
+
+  def increment_comments_counter
+    post.with_lock do
+      state = post.comments_counter || 0
+      post.comments_counter = state + 1
+      post.save
+    end
+  end
 end
