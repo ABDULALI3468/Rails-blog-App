6a816afbbb50b988d78789e44b26a9224b51a45e
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-21 18:35:06 +0500

    Added Users to Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# student = Student.create(
+#   firstname:'Ravi',
+#   lastname:'Singh',
+#   email:'ravi@bootcamp.com'
+# )
+
+40.times do|i|
+  puts"Creatingstudent#{i+1}"
+  User.create(name: "User #{i+1} ", photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: "Teacher from Country with code #{i+1}.", posts_counter: 0)
+end
+
+# Student.all.each do |student|
+#   student.blogs.create(title: "Dummy Blog for Student #{ student.id }", content: "Custom content pending")
+#   student.blogs.create(title: "Dummy Blog for Student #{ student.id }", content: "Custom content pending")
+# end
