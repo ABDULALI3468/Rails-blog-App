3d3a156dab9fcc5d42259819417ad934ba6e8645
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-24 16:02:45 +0500

    Added method to create a new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,28 @@ class PostsController < ApplicationController
     @post = @author.posts.find(params[:id])
   end
 
+  def create
+    @post = Post.create(post_params)
+    @post.user = current_user
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'New Post created successfully'
+          redirect_to user_posts_path(current_user)
+        else
+          flash.now[:error] = 'An error occurred : Post could not be created'
+        end
+      end
+    end
+  end
+
   private
 
   def fetch_author
     @author = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
